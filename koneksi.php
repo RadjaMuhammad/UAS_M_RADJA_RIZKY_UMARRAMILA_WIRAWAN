e46f53dd42800e376f4d8b549cb24abc3791b83b
RadjaMuhammad <rajarajaumar19@gmail.com>
2026-02-27 09:12:25 +0700

    tes

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "db_gadget_nim";
+
+    $koneksi = mysqli_connect("localhost", "root", "", "db_gadget_nim",);
+
+?>
